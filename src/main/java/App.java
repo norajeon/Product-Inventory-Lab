8d153fffe3142f43e5dd4e003395e29fb4b3c9d2
nora <sxj042@gmail.com>
2023-04-03 15:40:51 -0400

    part 1 and 2 done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,61 @@
-import services.SneakerService;
-
-public class App {
-    private SneakerService sneakerService = new SneakerService(); // (1)
-
-    public static void main(String... args){
-        App application = new App(); // (2)
-        application.init();  // (3)
-    }
-
-    public void init(){
-        // (4)
-        // application logic here
-        // call methods to take user input and interface with services
-        io.Console.printWelcome();
-    }
-}
+//import models.Sneaker;
+//import services.SneakerService;
+//import services.WhiskeyService;
+//
+//import java.util.InputMismatchException;
+//import java.util.Scanner;
+//
+//public class App {
+//    private SneakerService sneakerService = new SneakerService(); // (1)
+//    private WhiskeyService whiskeyService = new WhiskeyService();
+//
+//    Scanner input = new Scanner(System.in);
+//
+//    public static void main(String... args){
+//        App application = new App(); // (2)
+//        application.init();  // (3)
+//    }
+//
+//    public void init() {
+//        // (4)
+//        // application logic here
+//        // call methods to take user input and interface with services
+//        io.Console.printWelcome();
+//        boolean running = true;
+//
+//
+//        while (running) {
+//            try {
+//                String userInput = input.nextLine();
+//                System.out.println("Options:");
+//                switch (userInput) {
+//                    case "1":
+//                        addSneaker();
+//                        break;
+//                    case "2":
+//                        addWhiskey();
+//                        break;
+//                    case "exit":
+//                        running = false;
+//                        return;
+//                }
+//            } catch (InputMismatchException e) {
+//                System.out.println("\nInvalid.");
+//                input.next();
+//            }
+//
+//        } //while loop
+//
+//    } //init
+//
+//    public Sneaker addSneaker() {
+//        SneakerService sneak = new SneakerService();
+//        System.out.println("Options:");
+//        String userInput = input.nextLine();
+//
+//        return sneak.create();
+//
+//    }
+//
+//
+//}
--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,13 +1,13 @@
-package io;
-
-public class Console {
-    public static void printWelcome(){
-        System.out.println("" +
-                "**************************************************" +
-                "***           Welcome and Bienvenue            ***" +
-                "***                    to                      ***" +
-                "***          ZipCo Inventory Manager           ***" +
-                "**************************************************");
-    }
-
-}
+//package io;
+//
+//public class Console {
+//    public static void printWelcome(){
+//        System.out.println("" +
+//                "**************************************************" +
+//                "***           Welcome and Bienvenue            ***" +
+//                "***                    to                      ***" +
+//                "***          ZipCo Inventory Manager           ***" +
+//                "**************************************************");
+//    }
+//
+//}
