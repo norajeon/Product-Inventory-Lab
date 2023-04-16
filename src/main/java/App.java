231b588ef1132491152f655850a7c03647cf79ab
nora <sxj042@gmail.com>
2023-04-16 17:20:10 -0400

    updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,7 @@ public class App {
     private SneakerService sneakerService = new SneakerService(); // (1)
     private WhiskeyService whiskeyService = new WhiskeyService();
 
-    Scanner input = new Scanner(System.in);
+    private Console console = new Console();
 
     public static void main(String... args){
         Console.printWelcome();
@@ -25,10 +25,7 @@ public class App {
         boolean end = false;
         while (!end) {
             try {
-                System.out.println("1. Sneaker");
-                System.out.println("2. Whiskey");
-                System.out.println("'exit' to Exit");
-                String userInput = input.nextLine();
+                String userInput = console.getStringInput("1. Sneaker\n2.Whiskey\n'exit to Exit");
                 switch (userInput) {
                     case "1":
                         sneakerMenu();
@@ -41,8 +38,7 @@ public class App {
                         break;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("\nInvalid.");
-                input.next();
+                console.getStringInput("Invalid. Try Again. \n1. Sneaker\n2.Whiskey\n'exit to Exit");
             }
 
         } //while loop
@@ -53,27 +49,24 @@ public class App {
         boolean sneakerEnd = false;
         while (!sneakerEnd) {
             try {
-                System.out.println("1. Create Sneaker");
-                System.out.println("2. Find Sneaker by ID");
-                System.out.println("3. Find All Sneakers");
-                System.out.println("5. Delete Sneaker");
-                System.out.println("'exit' to Exit");
-                String choice = input.nextLine();
+                String choice = console.getStringInput("1. Create Sneaker\n" +
+                        "2. Find Sneaker by ID\n" +
+                        "3. Find All Sneakers\n" +
+                        "4. Delete Sneaker by ID\n" +
+                        "exit to Exit");
                 switch (choice) {
                     case "1":
                         addSneaker();
                         break;
                     case "2":
-                        System.out.println("Enter ID to check:");
-                        int check = input.nextInt();
+                        Integer check = console.getIntegerInput("Enter ID to check:");
                         findSneaker(check);
                         break;
                     case "3":
                         findAllSneakers();
                         break;
                     case "4":
-                        System.out.println("Enter ID to delete:");
-                        int deletion = input.nextInt();
+                        Integer deletion = console.getIntegerInput("Enter ID to delete:");
                         deleteSneaker(deletion);
                         break;
                     case "exit":
@@ -82,8 +75,11 @@ public class App {
                 }
             }
             catch (InputMismatchException e) {
-                System.out.println("\nInvalid Choice.");
-                input.next();
+               console.getStringInput("Invalid choice. \n1. Create Sneaker\n" +
+                        "2. Find Sneaker by ID\n" +
+                        "3. Find All Sneakers\n" +
+                        "4. Delete Sneaker by ID\n" +
+                        "exit to Exit");
             }
         }
     }
@@ -92,27 +88,24 @@ public class App {
         boolean whiskeyEnd = false;
         while (!whiskeyEnd) {
             try {
-                System.out.println("1. Create Whiskey");
-                System.out.println("2. Find Whiskey by ID");
-                System.out.println("3. Find All Whiskey");
-                System.out.println("5. Delete Whiskey");
-                System.out.println("'exit' to Exit");
-                String choice = input.nextLine();
+                String choice = console.getStringInput("1. Create Whiskey\n" +
+                        "2. Find Whiskey by ID\n" +
+                        "3. Find All Whiskey\n" +
+                        "4. Delete Whiskey by ID\n" +
+                        "exit to Exit");
                 switch (choice) {
                     case "1":
                         addWhiskey();
                         break;
                     case "2":
-                        System.out.println("Enter ID to check:");
-                        int check = input.nextInt();
+                        Integer check = console.getIntegerInput("Enter ID to check:");
                         findWhiskey(check);
                         break;
                     case "3":
                         findAllWhiskey();
                         break;
                     case "4":
-                        System.out.println("Enter ID to delete:");
-                        int deletion = input.nextInt();
+                        Integer deletion = console.getIntegerInput("Enter ID to delete:");
                         deleteWhiskey(deletion);
                         break;
                     case "exit":
@@ -121,25 +114,22 @@ public class App {
                 }
             }
             catch (InputMismatchException e) {
-                System.out.println("\nInvalid Choice.");
-                input.next();
+                console.getStringInput("Invalid choice. \n1. Create Whiskey\n" +
+                        "2. Find Whiskey by ID\n" +
+                        "3. Find All Whiskey\n" +
+                        "4. Delete Whiskey by ID\n" +
+                        "exit to Exit");
             }
         }
 
     }
     public Sneaker addSneaker() {
-        System.out.println("Enter name:");
-        String name = input.nextLine();
-        System.out.println("Enter brand:");
-        String brand = input.nextLine();
-        System.out.println("Enter sport");
-        String sport = input.nextLine();
-        System.out.println("Enter quantity:");
-        int qty = input.nextInt();
-        System.out.println("Enter price:");
-        float price = (float) input.nextFloat();
-        System.out.println("Enter size:");
-        double size = (double) input.nextDouble();
+        String name = console.getStringInput(" Enter name:");
+        String brand = console.getStringInput(" Enter brand:");
+        String sport = console.getStringInput(" Enter sport:");
+        Integer qty = console.getIntegerInput("Enter quantity:");
+        Float price = console.getFloatInput("Enter price:");
+        Double size = console.getDoubleInput("Enter size:");
 
         System.out.println("Sneaker created; name: " + name);
         return sneakerService.create(name, brand, sport, qty, price, size);
@@ -165,16 +155,12 @@ public class App {
 
 
     public Whiskey addWhiskey() {
-        WhiskeyService whisk = new WhiskeyService();
-        System.out.println("Enter name:");
-        String name = input.nextLine();
-        System.out.println("Enter brand:");
-        String brand = input.nextLine();
-        System.out.println("Enter price:");
-        double price = (double) input.nextFloat();
+        String name = console.getStringInput(" Enter name:");
+        String brand = console.getStringInput(" Enter brand:");
+        Double price = console.getDoubleInput("Enter price:");
 
         System.out.println("Whiskey created; name: " + name);
-        return whisk.create(name, brand, price);
+        return whiskeyService.create(name, brand, price);
     }
 
 
---
+++ src/main/java/CSVUtils.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+public class CSVUtils {
+    private static final char DEFAULT_SEPARATOR = ',';  // (1)
+
+    // (2)
+    public static void writeLine(Writer w, List<String> values) throws IOException {
+        boolean first = true;
+
+        StringBuilder sb = new StringBuilder();
+
+        // (3)
+        for (String value : values) {
+            if (!first) {
+                sb.append(DEFAULT_SEPARATOR);
+            }
+            sb.append(value);
+            first = false;
+        }
+        sb.append("\n");
+
+        w.append(sb.toString());  // (4)
+    }
+}
